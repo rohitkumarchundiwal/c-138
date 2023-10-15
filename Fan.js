f1a28c8604b0b1031ae88b824778af314793eb2a
rohitkumarchundiwal <124276493+rohitkumarchundiwal@users.noreply.github.com>
2023-10-15 13:06:30 +0530

    Add files via upload

---
+++ Fan.js
@@ -0,0 +1,30 @@
+Status = "";
+Fan_image = "";
+
+function preload() {
+    Fan_image = loadImage("Fan.jpg");
+}
+
+function setup() {
+    canvas = createCanvas(640,350);
+    canvas.position(315,200);
+    object_detector = ml5.objectDetector('cocossd',modelLoaded);
+    document.getElementById("status").innerHTML = "status: Detecting Objects";
+}
+
+function modelLoaded() {
+    console.log("Model Loaded!");
+    Status = true;
+    object_detector.detect(Fan_image,gotResults);
+}
+
+function gotResults(results,error){
+    if(error){
+        console.error(error);
+    }
+    console.log(results);
+}
+
+function draw(){
+    image(Fan_image,0,0,640,350);
+}
